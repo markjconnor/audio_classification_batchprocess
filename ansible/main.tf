1f8a41e003a9eeb6908f63ca2853c8526ef5e539
Mark Connor <ucabmjc@ucl.ac.uk>
2026-04-15 18:02:07 +0100

    Initial commit

---
+++ ansible/main.tf
@@ -0,0 +1,130 @@
+# Interpret namespace and network name based on user name
+locals {
+  namespace = "${var.username}-comp0235-ns"
+  network_name = "${var.username}-comp0235-ns/ds4eng"
+}
+
+data "harvester_image" "img" {
+  display_name = var.img_display_name
+  namespace    = "harvester-public"
+}
+
+data "harvester_ssh_key" "mysshkey" {
+  name      = var.keyname
+  namespace = local.namespace
+}
+
+resource "random_id" "secret" {
+  byte_length = 5
+}
+
+resource "harvester_cloudinit_secret" "cloud-config" {
+  name      = "cloud-config-${random_id.secret.hex}"
+  namespace = local.namespace
+
+  user_data = templatefile("cloud-init.tmpl.yml", {
+      public_key_openssh = data.harvester_ssh_key.mysshkey.public_key
+    })
+}
+
+resource "harvester_virtualmachine" "host" {
+  
+  count = 1
+
+  name                 = "${var.username}-host-${format("%02d", count.index + 1)}-${random_id.secret.hex}"
+  namespace            = local.namespace
+  restart_after_update = true
+
+  description = "Cluster Host Node"
+
+  cpu    = 2
+  memory = "10Gi"
+
+  efi         = true
+  secure_boot = false
+
+  run_strategy    = "RerunOnFailure"
+  hostname        = "${var.username}-host-${format("%02d", count.index + 1)}-${random_id.secret.hex}"
+  reserved_memory = "100Mi"
+  machine_type    = "q35"
+
+  network_interface {
+    name           = "nic-1"
+    wait_for_lease = true
+    type           = "bridge"
+    network_name   = local.network_name
+  }
+
+  disk {
+    name       = "rootdisk"
+    type       = "disk"
+    size       = "10Gi"
+    bus        = "virtio"
+    boot_order = 1
+
+    image       = data.harvester_image.img.id
+    auto_delete = true
+  }
+
+  cloudinit {
+    user_data_secret_name = harvester_cloudinit_secret.cloud-config.name
+  }
+
+  timeouts {
+    create = "20m"
+    update = "20m"
+    delete = "20m"
+  }
+
+}
+
+resource "harvester_virtualmachine" "workervm" {
+  
+  count = var.vm_count
+
+  name                 = "${var.username}-worker-${format("%02d", count.index + 1)}-${random_id.secret.hex}"
+  namespace            = local.namespace
+  restart_after_update = true
+
+  description = "Cluster Compute Node"
+
+  cpu    = 5
+  memory = "32Gi"
+
+  efi         = true
+  secure_boot = false
+
+  run_strategy    = "RerunOnFailure"
+  hostname        = "${var.username}-worker-${format("%02d", count.index + 1)}-${random_id.secret.hex}"
+  reserved_memory = "100Mi"
+  machine_type    = "q35"
+
+  network_interface {
+    name           = "nic-1"
+    wait_for_lease = true
+    type           = "bridge"
+    network_name   = local.network_name
+  }
+
+  disk {
+    name       = "rootdisk"
+    type       = "disk"
+    size       = "120Gi"
+    bus        = "virtio"
+    boot_order = 1
+
+    image       = data.harvester_image.img.id
+    auto_delete = true
+  }
+
+  cloudinit {
+    user_data_secret_name = harvester_cloudinit_secret.cloud-config.name
+  }
+
+  timeouts {
+    create = "20m"
+    update = "20m"
+    delete = "20m"
+  }
+
+}
---
+++ ansible/variables.tf
@@ -0,0 +1,19 @@
+variable img_display_name {
+  type = string
+  default = "AlmaLinux-9-GenericCloud-9.6-20250522"
+}
+
+variable username {
+  type = string
+  default = "ucabmjc"
+}
+
+variable keyname {
+  type = string
+  default = "cw-vm"
+}
+
+variable vm_count {
+  type    = number
+  default = 4
+}
