4fa9653016d50b9e666b8c7fb9c8ae1d279e5981
Mark Connor <ucabmjc@ucl.ac.uk>
2026-04-17 16:43:55 +0100

    Fix bugs

--- src/submit_bash_parallel.sh
+++ src/submit_bash_parallel.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=bash_parallel
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4          # We ask for 4 CPU cores
+#SBATCH --cpus-per-task=2          # TODO: Change this to correct number of CPU cores
 #SBATCH --array=0-99               # 100 total array jobs
-#SBATCH --output=/beegfs/dataset/logs/chunk_%a.out
+#SBATCH --output=/beegfs/almalinux/chunk_%a.out
 
 BATCH_SIZE=100
 MASTER_LIST="/beegfs/dataset/master_list.txt"
@@ -24,17 +24,17 @@ fi
 
 echo "Task $SLURM_ARRAY_TASK_ID: Processing files $start_idx to $end_idx"
 
-source /home/almalinux/comp0239_cw/venv/bin/activate
+source /home/almalinux/music-venv/bin/activate
 
 # The Parallel Loop
 for (( idx=start_idx; idx<=end_idx; idx++ )); do
     current_file=${files[$idx]}
     
     # Launch Python in the background (&)
-    /home/almalinux/comp0239_cw/music-venv/bin/python3 predict_single.py "$current_file" &
+    /home/almalinux/music-venv/bin/python3 predict_single.py "$current_file" &
 
     # Once we hit 4 parallel background jobs, wait for them to finish before continuing
-    if (( (idx - start_idx + 1) % 4 == 0 )); then
+    if (( (idx - start_idx + 1) % 2 == 0 )); then #TODO: Change this to 4 for 4 parallel jobs
         wait
     fi
 done
