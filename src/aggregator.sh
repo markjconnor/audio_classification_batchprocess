4b8abb765622370308f7cbb5d37cf501ef144886
Mark Connor <ucabmjc@ucl.ac.uk>
2026-04-19 23:51:11 +0100

    Fix path to aggregation python script

--- src/aggregator.sh
+++ src/aggregator.sh
@@ -3,12 +3,12 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1          # SQLite is single-threaded, 1 CPU is plenty
-#SBATCH --output=/beegfs/almalinux/aggregate.log
+#SBATCH --output=/home/almalinux/comp0239_cw/src/aggregate.log
 
 echo "Starting database aggregation..."
 source /home/almalinux/music-venv/bin/activate
 
 # Run the python script we created earlier
-/home/almalinux/music-venv/bin/python3 /beegfs/almalinux/aggregate_results.py
+/home/almalinux/music-venv/bin/python3 /home/almalinux/comp0239_cw/src/aggregate_results.py
 
 echo "Aggregation complete!"
